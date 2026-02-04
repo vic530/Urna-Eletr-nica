29e7526b7c9c0751cffff59dcbe1350b7340bec9
Vic530 <s.dantas.ti@gmail.com>
2026-02-04 01:41:00 -0300

    Lista-cadidatos

---
+++ Urna Eletronica/candidato.js
@@ -0,0 +1,64 @@
+const candidatos = [
+    {
+        cargo:'Herói do Ano',
+        caracteres: 2,
+        concorrentes: [
+            {
+                numero:'33',
+                nome: 'HOMEN ARANHA',
+                imagem: {url:'./assets/img/HOMEM-ARANHA.jpg'}
+            },
+
+            {
+                numero: '11',
+                nome: 'BATMAN',
+                imagem: {url: './assets/img/Batman.jpg'}
+            },
+
+            {
+                numero: '22',
+                nome: 'Mulher Maravilha',
+                imagem: {url: './assets/img/Mulher-Maravilha.webp'}
+            },
+
+             {
+                numero: '44',
+                nome: 'HULK',
+                imagem: {url: './assets/img/hulk.jpg'}
+            }
+
+        ]
+
+    },
+
+    {
+        cargo:'Vilão do Ano',
+        caracteres: 3,
+        concorrentes: [
+            {
+                numero: '202',
+                nome: 'Duende Verde',
+                imagem: {url:'./assets/img/Doende Verde.png'}
+            },
+            
+            {
+                numero: '303',
+                nome:'Curinga',
+                imagem: {url:'./assets/img/Curinga.jpg'}
+            },
+
+            {
+                numero: '101',
+                nome:'Mulher Gato',
+                imagem: {url:'./assets/img/mulher-gato.jpg'}
+            },
+
+            {
+                numero: '404',
+                nome:'THANOS',
+                imagem: {url:'./assets/img/thanos.jpg'}
+            },
+        ]
+    }
+
+]
